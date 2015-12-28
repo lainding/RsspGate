204dca012c9e0ef327543b8cb658735916959cb0
lainding <wding.cn@gmail.com>
2015-12-28 17:34:43 +0800

    2015-12-28

--- RsspGate/RsspGate/libs/Rssp1.cs
+++ RsspGate/RsspGate/libs/Rssp1.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace RsspGate.libs
 {
-    class Rssp1
-    {
-    }
-
     class RSD : DataFrame
     {
-        public byte ProtocolInteractionsCategory;
-        public byte FrameType;
-        public UInt16 SourceAddress;
-        public UInt16 DestinationAddress;
-        public UInt32 LocalCycleCounter;
-        public UInt16 DataLength;
+        public byte ProtocolInteractionsCategory;   // 协议交互类别
+        public byte FrameType;                      // 帧类型
+        public ushort SourceAddress;                // 源地址
+        public ushort DestinationAddress;           // 目的地址
+        public uint LocalCycleCounter;              // 本地周期计数器
+        public ushort DataLength;                   // 安全数据大小
+        public uint CRCM1;                          // 安全校验通道1
+        public uint CRCM2;                          // 安全校验通道2
+        public byte[] Data;                         // 应用数据
+        public ushort CRC16;                        // CRC16
 
 
         public override void DeSerialize()
@@ -26,6 +26,53 @@ namespace RsspGate.libs
         }
 
         public override void Serialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class SSE : DataFrame
+    {
+
+        public byte ProtocolInteractionsCategory;   // 协议交互类别
+        public byte FrameType;                      // 帧类型
+        public ushort SourceAddress;                // 源地址
+        public ushort DestinationAddress;           // 目的地址
+        public uint LocalCycleCounter;              // 本地周期计数器
+        public ulong SEQENQ_1;
+        public ulong SEQENQ_2;
+        public uint CRC16;
+
+        public override void DeSerialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Serialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class SSR : DataFrame
+    {
+        public byte ProtocolInteractionsCategory;   // 协议交互类别
+        public byte FrameType;                      // 帧类型
+        public ushort SourceAddress;                // 源地址
+        public ushort DestinationAddress;           // 目的地址
+        public uint LocalCycleCounter;
+        public uint RemoteCycleCounter;
+        public uint SEQENQ1;
+        public uint SEQENQ2;
+        public byte DateVersion;
+        public ushort CRC16;
+
+        public override void Serialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeSerialize()
         {
             throw new NotImplementedException();
         }
